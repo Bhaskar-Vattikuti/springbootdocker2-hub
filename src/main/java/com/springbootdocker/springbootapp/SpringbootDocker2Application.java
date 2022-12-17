e1872c4a95df923203d02996bd06b12806f9e4c3
Bhaskar-Vattikuti <bhaskarvattikuti4422@gmail.com>
2022-12-17 15:49:57 +0530

    first commit for file upload

---
+++ src/main/java/com/springbootdocker/springbootapp/SpringbootDocker2Application.java
@@ -0,0 +1,20 @@
+package com.springbootdocker.springbootapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class SpringbootDocker2Application {
+	@GetMapping
+	public String getmsg()
+	{
+		return "this is my first docker containers";
+	}
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootDocker2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/springbootdocker/springbootapp/SpringbootDocker2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootdocker.springbootapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootDocker2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
